90c48f171ada5484800038e3a47fe50a71df5b36
vincent.li <q315744068@gmail.com>
2024-06-16 22:30:41 +0800

    UPDATE: retrive url and filename from the command parameter instead of hardcode

--- first-rust/src/main.rs
+++ first-rust/src/main.rs
@@ -1,26 +1,17 @@
 use std::fs;
 
 fn main() {
-   fetch_rust_lang_as_md();
-    // println!("square of 25 is {}", apply(25, square));
-    // println!("cube of 25 is {}", apply(25, cube));
+   let args: Vec<String> =  std::env::args().collect();
+   let url = &args[1];
+   let file_to_write = &args[2];
+
+   let _ = fetch_rust_lang_as_md(url, file_to_write);
+
 }
 
-fn apply(value: i32, f: fn(i32) -> i32) -> i32 {
-    f(value)
-}
 
-fn square(value: i32) -> i32 {
-    value * value
-}
 
-fn cube(value: i32) -> i32 {
-    value * value * value
-}
-
-fn fetch_rust_lang_as_md() -> Result<(), Box<dyn std::error::Error>> {
-    let url = "https://www.rust-lang.org/";
-    let output = "rust.md";
+fn fetch_rust_lang_as_md(url: &str, file_to_write: &str) -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Fetching url: {}", url);
     let body = reqwest::blocking::get(url)?.text()?;
@@ -28,8 +19,8 @@ fn fetch_rust_lang_as_md() -> Result<(), Box<dyn std::error::Error>> {
     println!("Converting html to md...");
     let md = html2md::parse_html(&body);
 
-    fs::write(output,md.as_bytes())?;
-    println!("Converted markdown has been saved in {}.", output);
+    fs::write(file_to_write,md.as_bytes())?;
+    println!("Converted markdown has been saved in {}.", file_to_write);
 
     Ok(())
 }
