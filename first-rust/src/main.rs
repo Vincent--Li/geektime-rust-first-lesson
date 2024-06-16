cea1ac3c8947dd04aca84fde654cbe869c89775c
vincent.li <q315744068@gmail.com>
2024-06-16 19:53:37 +0800

    UPDATE: use ? instead of unwrap to handle the error, thus the error will pop up until being handled

--- first-rust/src/main.rs
+++ first-rust/src/main.rs
@@ -1,9 +1,9 @@
 use std::fs;
 
 fn main() {
-//    fetch_rust_lang_as_md();
-    println!("square of 25 is {}", apply(25, square));
-    println!("cube of 25 is {}", apply(25, cube));
+   fetch_rust_lang_as_md();
+    // println!("square of 25 is {}", apply(25, square));
+    // println!("cube of 25 is {}", apply(25, cube));
 }
 
 fn apply(value: i32, f: fn(i32) -> i32) -> i32 {
@@ -18,20 +18,18 @@ fn cube(value: i32) -> i32 {
     value * value * value
 }
 
-fn fetch_rust_lang_as_md() {
+fn fetch_rust_lang_as_md() -> Result<(), Box<dyn std::error::Error>> {
     let url = "https://www.rust-lang.org/";
     let output = "rust.md";
 
     println!("Fetching url: {}", url);
-    let body = reqwest::blocking::get(url)
-        .unwrap()
-        .text()
-        .unwrap();
+    let body = reqwest::blocking::get(url)?.text()?;
 
     println!("Converting html to md...");
     let md = html2md::parse_html(&body);
 
-    fs::write(output,md.as_bytes())
-        .unwrap();
+    fs::write(output,md.as_bytes())?;
     println!("Converted markdown has been saved in {}.", output);
+
+    Ok(())
 }
