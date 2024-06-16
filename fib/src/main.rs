97f0fa89e9d62ceafee8a2a05aecab8d28599f35
vincent.li <q315744068@gmail.com>
2024-06-16 22:10:58 +0800

    UPDATE: extract repeat part of fib

--- fib/src/main.rs
+++ fib/src/main.rs
@@ -1,12 +1,16 @@
+fn next(a: &mut i32, b: &mut i32) {
+    let c = *a + *b;
+    *a = *b;
+    *b = c;
+}
+
 fn fib_loop(n: u8) {
     let mut a = 1;
     let mut b = 1;
     let mut i = 2u8;
 
     loop {
-        let c = a + b;
-        a = b;
-        b = c;
+        next(&mut a, &mut b);
         i += 1;
 
         println!("next val is {}", b);
@@ -20,9 +24,7 @@ fn fib_loop(n: u8) {
 fn fib_while(n: u8) {
     let (mut a, mut b, mut i) = (1, 1, 2);
     while i < n {
-        let c = a + b;
-        a = b;
-        b = c;
+        next(&mut a, &mut b);
         i += 1;
         println!("next val is {}", b);
     }
@@ -31,9 +33,7 @@ fn fib_while(n: u8) {
 fn fib_for(n: u8) {
     let (mut a, mut b) = (1, 1);
     for _i in 2..n {
-        let c = a + b;
-        a = b;
-        b = c;
+        next(&mut a, &mut b);
         println!("next val is {}", b);
     }
 }
