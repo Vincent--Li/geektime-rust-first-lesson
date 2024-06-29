ee1c66323fc99d4d4a5fb19e64ee03ace3afc9a8
Vincent_li <q315744068@gmail.com>
2024-06-29 11:23:57 +0800

    UPDATE: RefCell 在静态编译的时候会放行, 但是在动态编译的时候,, 不让通过

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,12 @@
+use std::cell::RefCell;
+
 fn main() {
-    println!("Hello, world!");
+    let data = RefCell::new(1);
+
+    let mut v = data.borrow_mut();
+
+    *v += 1;
+
+    println!("data:{:?}", data.borrow()); // RefCell 静态编译能通过. 动态运行的时候, 会报错 违反了借规则"
+
 }
