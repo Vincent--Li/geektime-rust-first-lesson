340c8af2c8013f89e95d840ea4a6ec78870bff62
vincent.li <q315744068@gmail.com>
2024-06-30 10:56:57 +0800

    UPDATE: 新修改

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 fn main() {
     let mut data = 1;
     let v = &mut data;
@@ -5,3 +6,31 @@ fn main() {
     println!("data: {:?}", &data);
     println!("data: {:?}", v1);
 }
+=======
+use std::rc::Rc;
+
+#[derive(Debug)]
+struct Node {
+    id: usize,
+    downstream: Option<Rc<Node>>,
+}
+
+impl Node {
+    pub fn new(id: usize) -> Self {
+        Self {
+            id,
+            downstream: None,
+        }
+    }
+
+    pub fn update_downstream(&mut self, downstream: Rc<Node>) {
+        
+    }
+}
+
+fn main() {
+
+
+}
+
+>>>>>>> Stashed changes
