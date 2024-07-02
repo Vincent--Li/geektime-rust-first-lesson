ba795383ba57b3c85183a29a4d026f53ea6b3230
Vincent_li <q315744068@gmail.com>
2024-07-03 00:26:49 +0800

    UPDATE: 第三种写法, 依赖注入

--- type-sys/src/main.rs
+++ type-sys/src/main.rs
@@ -8,31 +8,19 @@ struct MyWriter<W> {
 }
 
 impl<W: Write> MyWriter<W> {
+
+    pub fn new(writer: W) -> Self {
+        MyWriter { writer }
+    }
+
     pub fn write(&mut self, buf: &str) -> std::io::Result<()> {
         self.writer.write_all(buf.as_bytes())
     }
 }
 
-impl MyWriter<BufWriter<TcpStream>> {
-    pub fn new(addr: &str) -> Self {
-        let stream = TcpStream::connect(addr).unwrap();
-        Self {
-            writer: BufWriter::new(stream),
-        }
-    }
-}
 
-impl MyWriter<File> {
-    pub fn new(addr: &str) -> Self {
-        let file = File::open(addr).unwrap();
-        Self { writer: file }
-    }
-}
 
 fn main() {
-    let mut writer = MyWriter::<BufWriter<TcpStream>>::new("127.0.0.1:8080");
-    let _ = writer.write("hello world!");
-
-    let mut writer = MyWriter::<File>::new("test.txt");
+    let mut writer = MyWriter::new(BufWriter::new(TcpStream::connect("127.0.0.1:8080").unwrap()));
     let _ = writer.write("hello world!");
 }
