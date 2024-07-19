08c0e163315f62ea4bca63f9b63f39908f62e7d3
Vincent_Li <q315744068@gmail.com>
2024-07-19 09:11:02 +0800

    rgrep command parse

--- rgrep/src/main.rs
+++ rgrep/src/main.rs
@@ -1,3 +1,41 @@
+use clap::Parser;
+use glob::glob;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// regexp to grep in file
+    params: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if args.params.len() == 0 {
+        println!("no params");
+        return;
+    }
+
+    if args.params.len() == 1 {
+        println!("regexp should be specified");
+        return;
+    }
+
+    let regexp = args.params[0].as_str();
+    let filename = args.params[1].as_str();
+    println!("regexp: {}, filename: {}", regexp, filename);
+
+    // glob files
+    for entry in glob(filename).expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path) => {
+                println!("path: {}", path.display());
+            },
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
+
 }
