d0df3934f71ddbd9b0718126c1c1c4a708d43602
Vincent_Li <q315744068@gmail.com>
2024-07-23 21:19:45 +0800

    inner loop for 5 times and free the mutext, then main thread will wait for the condvar to be true to go on

--- concurrency-condvar/src/main.rs
+++ concurrency-condvar/src/main.rs
@@ -1,3 +1,29 @@
+use std::sync::{Arc, Condvar, Mutex};
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let pair = Arc::new((Mutex::new(false), Condvar::new()));
+    let pair2 = Arc::clone(&pair);
+
+    thread::spawn(move || {
+        let (lock, cvar) = &*pair2;
+        let mut started = lock.lock().unwrap();
+        *started = true;
+        eprintln!("I'm a happy worker!");
+        // 通知主线程
+        cvar.notify_one();
+        for i in 1..5  {
+            thread::sleep(Duration::from_secs(1));
+            println!("working loop {}...", i);
+        }
+    });
+
+    // 等待工作线程的通知
+    let (lock, cvar) = &*pair;
+    let mut started = lock.lock().unwrap();
+    while !*started {
+        started = cvar.wait(started).unwrap();
+    }
+    eprintln!("Worker started!");
 }
