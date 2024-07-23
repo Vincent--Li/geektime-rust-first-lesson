01fcde60781e3a3b8a2f8eaacd0b317fc6c47667
Vincent_Li <q315744068@gmail.com>
2024-07-23 20:41:51 +0800

    UPDATE: lock free Usage of Metrics, because the lock reside in the METRICS inner data

--- concurrency-in-memory-metric/src/main.rs
+++ concurrency-in-memory-metric/src/main.rs
@@ -1,3 +1,37 @@
+mod metric;
+
+
+use std::thread;
+
+use lazy_static::lazy_static;
+use metric::Metrics;
+
+lazy_static! {
+    pub(crate) static ref METRICS: Metrics = Metrics::new(&[
+        "topics",
+        "clients",
+        "peers",
+        "broadcasts",
+        "servers",
+        "states",
+        "subscribers"
+    ]);
+}
+
 fn main() {
-    println!("Hello, world!");
+    METRICS.inc("topics");
+    METRICS.inc("subscribers");
+
+    println!("{:?}", METRICS.snapshot());
+
+    let t1 = thread::spawn(|| {
+        METRICS.inc("peers");
+    });
+    let t2 = thread::spawn(|| {
+        METRICS.dec("subscribers");
+    });
+
+    t1.join().unwrap();
+    t2.join().unwrap();
+    println!("{:?}", METRICS.snapshot());
 }
